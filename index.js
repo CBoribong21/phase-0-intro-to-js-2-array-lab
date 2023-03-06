ec682581976f7efc8c172dcd01562e5ecc7e2d98
Christina Boribong <CPBoribong@gmail.com>
2023-03-05 19:36:24 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+function appendCat(name) {
+    return [...cats,name];
+}
+function prependCat(name){
+  return [name,...cats];  
+}
+function removeLastCat(name) {
+    return cats.slice(0,-1);
+}
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
